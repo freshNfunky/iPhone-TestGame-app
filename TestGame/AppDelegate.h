5c5c5b6326d0e6253a350dae39142725c7cf38ce
freshNfunky <hallo@felixschaller.com>
2016-06-16 22:04:10 +0200

    Initial Commit

---
+++ TestGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGame
+//
+//  Created by felix schaller on 16.06.16.
+//  Copyright © 2016 felix schaller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGame/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  TestGame
+//
+
+//  Copyright (c) 2016 felix schaller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
